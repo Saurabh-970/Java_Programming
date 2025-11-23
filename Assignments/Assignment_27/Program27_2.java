263ec4210d1344dd4fc0524bf0edf62660fa804e
Saurabh Bhonsle <141349409+Saurabh-970@users.noreply.github.com>
2025-11-23 18:33:49 +0530

    Add files via upload

---
+++ Assignments/Assignment_27/Program27_2.java
@@ -0,0 +1,70 @@
+import java.util.*;
+
+///////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function name : Display
+//  Description   : Accept number of rows and columns and display in star patter
+//  Input         : Integer
+//  Output        : Integer with special character
+//  Author        : Saurabh Ravindra Bhonsle
+//  Date          : 20 /11/2025
+//
+///////////////////////////////////////////////////////////////////////////////////////////
+
+class Pattern
+{
+    public void  Display(int iRow, int iCol)
+    {
+        int iCnt = 0;
+        int jCnt = 0;
+
+        for(iCnt =1; iCnt <= iRow; iCnt++)
+        {
+            for(jCnt = 1; jCnt <= iCol ; jCnt++)
+            {
+                System.out.printf("%d\t",+jCnt);
+            }
+            System.out.println();
+        }        
+
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Entry point function of the application
+//
+///////////////////////////////////////////////////////////////////////////////////////////
+
+class Program27_2
+{
+    public static void main(String A[])
+    {
+        int iValue1 = 0;
+        int iValue2 = 0;
+
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter number rows :");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter number columns :");
+        iValue2 = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+        
+        pobj.Display(iValue1,iValue2);
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Testcases successfully handled by the application
+//
+//    Enter number of rows and columns   : 4 3
+//                                              
+//    Output :  1       2       3
+//              1       2       3
+//              1       2       3
+//              1       2       3                 
+///////////////////////////////////////////////////////////////////////////////////////////
---
+++ Assignments/Assignment_27/Program27_4.java
@@ -0,0 +1,78 @@
+import java.util.*;
+
+///////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function name : Display
+//  Description   : Accept number of rows and columns and display in star patter
+//  Input         : Integer
+//  Output        : Integer with special character
+//  Author        : Saurabh Ravindra Bhonsle
+//  Date          : 20 /11/2025
+//
+///////////////////////////////////////////////////////////////////////////////////////////
+
+class Pattern
+{
+    public void  Display(int iRow, int iCol)
+    {
+        int iCnt = 0;
+        int jCnt = 0;
+
+        for(iCnt = 1; iCnt <= iRow; iCnt++)
+        {
+            for(jCnt = 1; jCnt <= iCol ; jCnt++)
+            {
+                if(jCnt % 2 == 1)
+                {
+                    System.out.print("*\t");
+                }
+                else
+                {
+                    System.out.print("#\t");
+                }
+                
+            }
+            System.out.println();
+        }        
+
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Entry point function of the application
+//
+///////////////////////////////////////////////////////////////////////////////////////////
+
+class Program27_4
+{
+    public static void main(String A[])
+    {
+        int iValue1 = 0;
+        int iValue2 = 0;
+
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter number rows :");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter number columns :");
+        iValue2 = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+        
+        pobj.Display(iValue1,iValue2);
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Testcases successfully handled by the application
+//
+//    Enter number of rows and columns   : 3 4
+//                                              
+//    Output :  *       #       *       #
+//              *       #       *       #
+//              *       #       *       #            
+//                  
+///////////////////////////////////////////////////////////////////////////////////////////
